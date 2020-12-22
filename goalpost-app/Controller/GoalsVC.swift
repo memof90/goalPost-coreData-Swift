4b1aeaa67ec72ddef5bb881c0425f399edd924ed
memo figueredo <memof80@gmail.com>
2020-12-22 13:08:02 -0500

    add first controller and first view GoalsVC

---
+++ goalpost-app/Controller/GoalsVC.swift
@@ -0,0 +1,30 @@
+//
+//  GoalsVC.swift
+//  goalpost-app
+//
+//  Created by Memo Figueredo on 22/12/20.
+//
+
+import UIKit
+
+class GoalsVC: UIViewController {
+    
+//    MARK: IBOutlet
+//    tableView
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    //    MARK: IBAction
+//    button addgoals
+    @IBAction func addGoAlbtnWasPressed(_ sender: Any) {
+    }
+    
+}
+
